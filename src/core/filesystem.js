587ffb2b593bc2332ed6ab23f1c468a7b10106f9
Shalitha Suranga <shalithasuranga@gmail.com>
2018-05-21 18:47:39 +0530

    Add filesytem functions

--- src/core/filesystem.js
+++ src/core/filesystem.js
@@ -1,3 +1,5 @@
+import { read } from 'fs';
+
 let $ = require('../lib/minAjax.js');
 
 let createDirectory = (dirName, s, e) => {
@@ -19,6 +21,85 @@ let createDirectory = (dirName, s, e) => {
 };
 
 
+let removeDirectory = (dirName, s, e) => {
+    $.ajax({
+        url : '/filesystem/removeDirectory',
+        type : 'POST',
+        data : {
+          name : dirName
+        },
+        success : function(data){
+            s(data);
+        },
+        errorCallback : () => {
+            e();
+        }
+    
+    });
+
+};
+
+let writeFile = (fileName, content, s, e) => {
+    $.ajax({
+        url : '/filesystem/writeFile',
+        type : 'POST',
+        data : {
+          name : fileName,
+          content : content
+        },
+        success : function(data){
+            s(data);
+        },
+        errorCallback : () => {
+            e();
+        }
+    
+    });
+
+};
+
+let readFile = (fileName, s, e) => {
+    $.ajax({
+        url : '/filesystem/readFile',
+        type : 'POST',
+        data : {
+          name : fileName
+        },
+        success : function(data){
+            s(data);
+        },
+        errorCallback : () => {
+            e();
+        }
+    
+    });
+
+};
+
+
+let removeFile = (fileName, s, e) => {
+    $.ajax({
+        url : '/filesystem/removeFile',
+        type : 'POST',
+        data : {
+          name : fileName
+        },
+        success : function(data){
+            s(data);
+        },
+        errorCallback : () => {
+            e();
+        }
+    
+    });
+
+};
+
+
 module.exports = {
-    createDirectory : createDirectory
+    createDirectory : createDirectory,
+    removeDirectory : removeDirectory,
+    writeFile : writeFile,
+    readFile : readFile,
+    removeFile : removeFile
 }
