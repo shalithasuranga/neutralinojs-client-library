6d5d399ce8fdeca317026c9fc2a3e51b382e33bd
Shalitha Suranga <shalithasuranga@gmail.com>
2018-05-21 20:36:59 +0530

    Add init script

--- src/index.js
+++ src/index.js
@@ -2,10 +2,12 @@ let filesystem = require('./core/filesystem');
 let settings = require('./core/settings');
 let os = require('./core/os');
 let computer = require('./core/computer');
+let init = require('./core/init');
 
 module.exports =  {
     filesystem : filesystem,
     settings : settings,
     os : os,
-    computer : computer
+    computer : computer,
+    init : init
 }
