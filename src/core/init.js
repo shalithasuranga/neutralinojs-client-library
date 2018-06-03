1c0562d0ff5a788035616f962496a51b6eb0562d
Shalitha Suranga <shalithasuranga@gmail.com>
2018-06-03 12:44:30 +0530

    Add init

--- src/core/init.js
+++ src/core/init.js
@@ -1,17 +1,12 @@
 
 module.exports =  (options) => {
-    if(options.fullScreen) {
-        let element = document.documentElement;
-
-        if(element.requestFullscreen) {
-            element.requestFullscreen();
-        } else if(element.mozRequestFullScreen) {
-            element.mozRequestFullScreen();
-        } else if(element.webkitRequestFullscreen) {
-            element.webkitRequestFullscreen();
-        } else if(element.msRequestFullscreen) {
-            element.msRequestFullscreen();
-        }
-            
+    document.body.style.visibility = 'hidden';
+    if(options.load) {
+       document.onreadystatechange = () => {
+           if(document.readyState === 'complete') {
+               options.load();
+               document.body.style.visibility = '';
+           }
+       }; 
     }
 }
