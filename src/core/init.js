b9b26b53e2a8d4a3a85fa56c6b145386269ea86d
Shalitha <shalithasuranga@gmail.com>
2018-06-03 14:34:17 +0530

    Remove dom event from lib

--- src/core/init.js
+++ src/core/init.js
@@ -1,12 +1,6 @@
 
 module.exports =  (options) => {
-    document.body.style.visibility = 'hidden';
     if(options.load) {
-       document.onreadystatechange = () => {
-           if(document.readyState === 'complete') {
-               options.load();
-               document.body.style.visibility = '';
-           }
-       }; 
+        options.load();
     }
 }
